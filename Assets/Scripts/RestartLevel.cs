d972926077da4fe49bfc119a3adf6c639797f439
A. Emre Ünal <aemreunal@gmail.com>
2013-08-26 21:53:31 +0300

    Fixed a serious error. Have accidentally deleted a used script, replaced it.

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartLevel : MonoBehaviour {
+	public int waitTime = 3;
+	
+	private bool restartInitiated = false;
+
+	void Update () {
+		if(!restartInitiated && transform.position.z > 11) {
+			restartInitiated = true;
+			StartCoroutine("Restart");
+		}
+	}
+	
+	IEnumerator Restart() {
+		yield return new WaitForSeconds(waitTime);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
